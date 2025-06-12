26bb51876df08331b132d7d4f3dc7de313b17524
DarkLord-13 <nishantrajendrakumar@gmail.com>
2025-06-13 01:41:04 +0530

    first commit

--- src/main/java/com/toDoList/toDoList/entity/listItem.java
+++ src/main/java/com/toDoList/toDoList/entity/listItem.java
@@ -1,4 +1,28 @@
 package com.toDoList.toDoList.entity;
 
-public class listItem {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "list_items", schema = "todo")
+public class listItem{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "task_name", nullable = false)
+    private String taskName;
+
+    @Column(name = "task_completed", nullable = false, columnDefinition = "boolean default false")
+    private boolean taskCompleted;
+
+    public listItem(String taskName){
+        this.taskName = taskName;
+        this.taskCompleted = false;
+    }
 }
