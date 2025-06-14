6b45ded5f8ce03d2797670d8e24d731b8b83c62d
DarkLord-13 <nishantrajendrakumar@gmail.com>
2025-06-14 17:58:29 +0530

    changed entity attributes and empty added controller files

--- src/main/java/com/toDoList/toDoList/repository/listRepository.java
+++ src/main/java/com/toDoList/toDoList/repository/listRepository.java
@@ -2,9 +2,9 @@ package com.toDoList.toDoList.repository;
 
 import com.toDoList.toDoList.entity.listItem;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
 public interface listRepository extends JpaRepository<listItem, Long>{
 
-    // List<listItem> findByTaskStatus(String status);
+    List<listItem> findByTaskStatus(String status);
 }
